737c0a131653246066f73f7362dea9201f532b81
Elliot Bulmer <egbulmer@icloud.com>
2024-12-01 15:09:19 +0800

    Refactor to use a helper test function

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -1,38 +1,15 @@
 package day1
 
 import (
-	"os"
-	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/egbulmer/advent24/testutil"
 )
 
-var sampleInput = `
-3   4
-4   3
-2   5
-1   3
-3   9
-3   3
-`
-
 func TestPuzzle1Sample(t *testing.T) {
-	r := strings.NewReader(sampleInput)
-	n, err := Puzzle1(r)
-
-	require.NoError(t, err)
-	require.Equal(t, 11, n)
+	testutil.TestPuzzle(t, Puzzle1, "sample.txt", 11)
 }
 
 func TestPuzzle1(t *testing.T) {
-	f, err := os.Open("input.txt")
-	defer f.Close()
-
-	require.NoError(t, err)
-
-	n, err := Puzzle1(f)
-
-	require.NoError(t, err)
-	require.Equal(t, 1660292, n)
+	testutil.TestPuzzle(t, Puzzle1, "input.txt", 1660292)
 }
---
+++ testutil/testutil.go
@@ -0,0 +1,22 @@
+package testutil
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type PuzzleFunc func(io.Reader) (int, error)
+
+func TestPuzzle(t *testing.T, puzzle PuzzleFunc, filename string, want int) {
+	f, err := os.Open(filename)
+	defer f.Close()
+	require.NoError(t, err)
+
+	answer, err := puzzle(f)
+
+	require.NoError(t, err)
+	require.Equal(t, want, answer)
+}
