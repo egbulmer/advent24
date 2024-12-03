3440645881bad7b796eb19c027ad90f4598013b5
Elliot Bulmer <egbulmer@icloud.com>
2024-12-03 17:12:27 +0800

    Add template

---
+++ template/day.go
@@ -0,0 +1,14 @@
+package template
+
+import (
+	"errors"
+	"io"
+)
+
+func Puzzle1(r io.Reader) (int, error) {
+	return 0, errors.New("unsolved")
+}
+
+func Puzzle2(r io.Reader) (int, error) {
+	return 0, errors.New("unsolved")
+}
